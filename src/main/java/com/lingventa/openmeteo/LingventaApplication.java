124ea3244e181184a11f9a9bdfd40fc91adebdc2
przemekwrona <przemekwrona25@gmail.com>
2023-02-27 13:09:38 +0100

    init project

---
+++ src/main/java/com/lingventa/openmeteo/LingventaApplication.java
@@ -0,0 +1,13 @@
+package com.lingventa.openmeteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingventaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lingventa/openmeteo/LingventaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingventa.openmeteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
