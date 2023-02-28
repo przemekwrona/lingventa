4d8d06bdfe84158a4e4cd9809bcfba1eb71ace09
przemekwrona <przemekwrona25@gmail.com>
2023-02-28 12:38:07 +0100

    feat: define external API Interface

--- src/main/java/com/lingventa/openmeteo/statistic/StatisticController.java
+++ src/main/java/com/lingventa/openmeteo/statistic/StatisticController.java
@@ -1,8 +1,8 @@
 package com.lingventa.openmeteo.statistic;
 
+import com.lingventa.api.StatisticsApi;
+import com.lingventa.api.model.MeteoStatistic;
 import lombok.AllArgsConstructor;
-import org.openapitools.api.StatisticsApi;
-import org.openapitools.model.MeteoStatistic;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
